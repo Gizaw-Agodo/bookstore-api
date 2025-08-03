3164e90e5922d1178514d2443dac0870f4542668
Gizaw <gizawag123@gmail.com>
2025-08-03 23:29:50 +0300

    add auth route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,36 @@
 from fastapi import FastAPI
-from api import book_routes
+from app.api.book_routes import book_router
+from app.api.auth_routes import auth_router
+# from app.core.database import create_db
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    print('server is starting...')
+    # await create_db()
+    try:
+        pass
+    except:
+        pass
+
+    yield 
+
+    print('server has been stoped.')
 
 
-app = FastAPI(docs_url='/docs')
-app.include_router(book_routes.router, prefix='api/vl')
+
+
+version = "v1"
+app = FastAPI(
+    version=version,
+    docs_url='/docs',
+    title="Bookly api", 
+    description=" a rest api for book review web service", 
+    lifespan= lifespan
+)
+
+
+
+# add routes
+app.include_router(book_router, prefix=f'/api/{version}/books', tags=['Books'])
+app.include_router(auth_router, prefix=f'/api/{version}/auth', tags=['Auth'])
